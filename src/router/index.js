300dbb4ecc6bc52a148549a81e9861a097a88d70
Jin4869 <1350875311@qq.com>
2022-11-22 22:33:14 +0800

    框架的修改和标签管理的静态网页实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -32,150 +32,261 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true,
   },
 
   {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true,
   },
 
   {
-    path: '/',
-    component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
-    }]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
+    path: "/",
+    component: Layout, //src/views/
     children: [
       {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        path: "home/index",
+        name: "home",
+        component: () => import("@/views/dashboard/admin/index"),
+        meta: { title: "首页", icon: "el-icon-s-home" },
+        //icon：图标
       },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
+    ],
   },
 
   {
-    path: '/form',
-    component: Layout,
+    path: "/",
+    component: Layout, //src/views/
     children: [
       {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  },
-
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+        path: "users_management/index",
+        name: "Users_management",
+        component: () => import("@/views/users_management/index"),
+        meta: { title: "用户管理", icon: "el-icon-s-custom" },
+        //icon：图标
       },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
-      }
-    ]
+    ],
   },
 
   {
-    path: 'external-link',
-    component: Layout,
+    path: "/",
+    component: Layout, //src/views/
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
+        path: "posts_management/index",
+        name: "Posts_management",
+        component: () => import("@/views/posts_management/index"),
+        meta: { title: "帖子管理", icon: "el-icon-document" },
+        //icon：图标
+      },
+    ],
   },
+
+  {
+    path: "/",
+    component: Layout, //src/views/
+    children: [
+      {
+        path: "label_management/index",
+        name: "Label_management",
+        component: () => import("@/views/label_management/index"),
+        meta: { title: "标签管理", icon: "el-icon-collection-tag" },
+        //icon：图标
+      },
+    ],
+  },
+
+  {
+    path: "/",
+    component: Layout, //src/views/
+    children: [
+      {
+        path: "business/index",
+        name: "business",
+        component: () => import("@/views/business/index"),
+        meta: { title: "推广链接管理", icon: "el-icon-share" },
+        //icon：图标
+      },
+    ],
+  },
+
+  {
+    path: "/",
+    component: Layout, //src/views/
+    children: [
+      {
+        path: "personal_center/index",
+        name: "Personal_center",
+        component: () => import("@/views/personal_center/index"),
+        meta: { title: "个人中心", icon: "el-icon-user-solid" },
+        //icon：图标
+      },
+    ],
+  },
+
+  // {
+  //   path: '/home',
+  //   component: Layout,
+  //   redirect: '/home',
+  //   name: 'Home',
+  //   meta: { title: '首页', icon: 'el-icon-s-help' },
+  //   children: [
+  //     {
+  //       path: '/home/chart_data',
+  //       name: 'chart_data',
+  //       component: () => import('@/views/home/chart_data/index'),
+  //       meta: { title: '图表数据', icon: 'table' }
+  //     }
+  //   ]
+  // },
+
+  // {
+  //   path: '/chart_data',
+  //   component: Layout,
+  //   redirect: '/chart_data',
+  //   name: 'Chart_data',
+  //   meta: { title: '图表数据', icon: 'el-icon-s-help' },
+  //   children: [
+  //     {
+  //       path: '/publication_data',
+  //       name: 'Publication_data',
+  //       component: () => import('@/views/chart_data/publication_data/index'),
+  //       meta: { title: '文章发表数据', icon: 'table' }
+  //     },
+  //     {
+  //       path: '/browsing_data',
+  //       name: 'browsing_data',
+  //       component: () => import('@/views/chart_data/browsing_data/index'),
+  //       meta: { title: '文章浏览数据', icon: 'tree' }
+  //     }
+  //   ]
+  // },
+
+  // {
+  //   path: '/example',
+  //   component: Layout,
+  //   redirect: '/example/table',
+  //   name: 'Example',
+  //   meta: { title: 'Example', icon: 'el-icon-s-help' },
+  //   children: [
+  //     {
+  //       path: 'table',
+  //       name: 'Table',
+  //       component: () => import('@/views/table/index'),
+  //       meta: { title: 'Table', icon: 'table' }
+  //     },
+  //     {
+  //       path: 'tree',
+  //       name: 'Tree',
+  //       component: () => import('@/views/tree/index'),
+  //       meta: { title: 'Tree', icon: 'tree' }
+  //     }
+  //   ]
+  // },
+
+  // {
+  //   path: '/form',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'index',
+  //       name: 'Form',
+  //       component: () => import('@/views/form/index'),
+  //       meta: { title: 'Form', icon: 'form' }
+  //     }
+  //   ]
+  // },
+
+  // {
+  //   path: '/nested',
+  //   component: Layout,
+  //   redirect: '/nested/menu1',
+  //   name: 'Nested',
+  //   meta: {
+  //     title: 'Nested',
+  //     icon: 'nested'
+  //   },
+  //   children: [
+  //     {
+  //       path: 'menu1',
+  //       component: () => import('@/views/nested/menu1/index'), // Parent router-view
+  //       name: 'Menu1',
+  //       meta: { title: 'Menu1' },
+  //       children: [
+  //         {
+  //           path: 'menu1-1',
+  //           component: () => import('@/views/nested/menu1/menu1-1'),
+  //           name: 'Menu1-1',
+  //           meta: { title: 'Menu1-1' }
+  //         },
+  //         {
+  //           path: 'menu1-2',
+  //           component: () => import('@/views/nested/menu1/menu1-2'),
+  //           name: 'Menu1-2',
+  //           meta: { title: 'Menu1-2' },
+  //           children: [
+  //             {
+  //               path: 'menu1-2-1',
+  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+  //               name: 'Menu1-2-1',
+  //               meta: { title: 'Menu1-2-1' }
+  //             },
+  //             {
+  //               path: 'menu1-2-2',
+  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+  //               name: 'Menu1-2-2',
+  //               meta: { title: 'Menu1-2-2' }
+  //             }
+  //           ]
+  //         },
+  //         {
+  //           path: 'menu1-3',
+  //           component: () => import('@/views/nested/menu1/menu1-3'),
+  //           name: 'Menu1-3',
+  //           meta: { title: 'Menu1-3' }
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       path: 'menu2',
+  //       component: () => import('@/views/nested/menu2/index'),
+  //       name: 'Menu2',
+  //       meta: { title: 'menu2' }
+  //     }
+  //   ]
+  // },
+
+  // {
+  //   path: 'external-link',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+  //       meta: { title: 'External Link', icon: 'link' }
+  //     }
+  //   ]
+  // },
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
-]
+  { path: "*", redirect: "/404", hidden: true },
+];
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
---
+++ src/views/label_management/index.vue
@@ -0,0 +1,208 @@
+<template>
+  <section>
+    <div>
+      <el-card>
+        <el-table
+          :data="
+            tableData.slice(
+              (currentPage - 1) * pageSize,
+              currentPage * pageSize
+            )
+          "
+          stripe
+          border="true"
+          style="width: 100%"
+          max-height="450"
+          highlight-current-row
+          @current-change="handleCurrentChange"
+          :default-sort="{ prop: 'date', order: 'descending' }"
+          @sort-change="changeTableSort"
+          :cell-style="{ 'text-align': 'center' }"
+          :header-cell-style="{ 'text-align': 'center' }"
+        >
+          <el-table-column type="index" width="50"> </el-table-column>
+          <el-table-column prop="date" label="创建时间" sortable width="180">
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.date }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="name" label="创建的人" width="180">
+          </el-table-column>
+          <el-table-column
+            prop="num"
+            label="与标签关联的帖子数量"
+            sortable
+            width="200"
+          >
+          </el-table-column>
+          <el-table-column prop="id" label="ID"> </el-table-column>
+          <el-table-column prop="call" label="名称" min-width="180">
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </div>
+    <el-card class="pag">
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[8, 10, 16]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
+    </el-card>
+  </section>
+</template>
+
+<style>
+.pag {
+  text-align: right;
+  line-height: 35px;
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.currentPage = 1;
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+    },
+  },
+  data() {
+    return {
+      currentPage: 1, //当前页数
+      pageSize: 8,
+      total: 17,
+      tableData: [
+        {
+          date: "2022-11-02",
+          name: "王小虎",
+          num: 110,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 11,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-21",
+          name: "王小虎",
+          num: 124,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-20",
+          name: "王小虎",
+          num: 90,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-21",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-19",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-18",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+        {
+          date: "2022-11-03",
+          name: "王小虎",
+          num: 3,
+          id: "1234",
+          call: "解封",
+        },
+      ],
+    };
+  },
+};
+</script>
