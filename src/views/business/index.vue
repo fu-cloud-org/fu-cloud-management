f30a34473d2f19b6d57442eed9bf65d8be11d680
032004129xuzhiyong <1643586262@qq.com>
2022-11-24 16:48:39 +0800

    添加dialog

--- src/views/business/index.vue
+++ src/views/business/index.vue
@@ -5,19 +5,32 @@
     stripe 
     style="width:100%;border-radius: 12px"
     :default-sort="{prop:'title',order:'descending'}"
-    
     >
-      <el-table-column prop="title" label="Title" width="250" sortable />
-      <el-table-column prop="link" label="Link" width="250" :show-overflow-tooltip="true"/>
+      <el-table-column  label="Title" width="250" sortable>
+        <template #default="scope">
+            <el-input v-model="editTitle" placeholder="Edit Title" v-if="isEdit" />
+            <span v-else>{{scope.row.title}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="Link" width="250" :show-overflow-tooltip="true">
+        <template #default="scope">
+            <el-input v-model="editLink" placeholder="Edit Link" v-if="isEdit" />
+            <span v-else>{{scope.row.link}}</span>
+        </template>
+      </el-table-column>
       <el-table-column align="right">
         <template #header>
             <el-input v-model="search" size="large" placeholder="Type to search" />
         </template>
         <template #default="scope">
-            <el-button size="small" @click="handleEdit(scope.$index, scope.row)"> 
+            <el-button v-if="isEdit" size="small" @click="handConfirm">
+                Confirm
+            </el-button>
+            <el-button v-show="!isEdit" size="small" @click="handleEdit(scope.$index, scope.row)"> 
                 Edit
             </el-button>
             <el-button
+                v-show="!isEdit"
                 size="small"
                 type="danger"
                 @click="handleDelete(scope.$index, scope.row)"
@@ -26,26 +39,32 @@
         </template>
       </el-table-column>
     </el-table>
-    <el-button  style="width:100%" @click="onAddItem" type="success">Add Item</el-button>
-    <div v-show="isShow">
-        <div class="mask">
-            <div class="diag">
-                <el-form :model="form" label-width="100px">
-                    <el-form-item label="Title">
-                        <el-input v-model="form.title" />
-                    </el-form-item>
-                    <el-form-item label="Link">
-                        <el-input v-model="form.link" />
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="success" @click="onSubmit"> Confirm </el-button>
-                        <el-button type="primary" @click="close"> Cancel </el-button>
-                    </el-form-item>
-                </el-form>
-  
-            </div>
-        </div>
-    </div>
+    <el-button  style="width:100%" @click="handAddItem" type="success">Add Item</el-button>
+    <el-dialog
+    :visible.sync="isShow"
+    width="40%"
+    center
+    :before-close="handDialogCancel"
+    >
+    <template #title>
+        <span class="el-icon-plus">添加</span>
+    </template>
+    <template #default>
+        <el-form :model="form" label-width="50px">
+            <el-form-item label="Title">
+                <el-input v-model="form.title" />
+            </el-form-item>
+            <el-form-item label="Link">
+                <el-input v-model="form.link" />
+            </el-form-item>
+        </el-form>
+    </template>
+    <template #footer>
+        <el-button type="success" @click="handDialogConfirm">确 定</el-button>
+        <el-button type="primary" @click="handDialogCancel">取 消</el-button>
+    </template>
+
+</el-dialog>
     </div>
   </template>
   
@@ -53,6 +72,9 @@
   export default{
     data(){
        return {
+        editTitle:"",
+        editLink:"",
+        isEdit:false,
         form:{
             title:'',
             link:''
@@ -86,20 +108,39 @@
     methods:{
         handleEdit(index,row){
             console.log(index, row.link)
+            this.isEdit=true
         },
         handleDelete(index,row){
             console.log(index, row)
-            this.tableData.splice(index, 1)
+            this.tableData = this.tableData.filter((item)=>{
+                return item.title !== row.title
+            })
         },
-        onAddItem(){
+        handConfirm(){
+            this.isEdit = false;
+        },
+        handAddItem(){
             this.isShow = true;
-  
         },
-        close(){
+        handDialogCancel(){
             this.isShow = false;
         },
-        onSubmit(){
-            this.isShow = false
+        handDialogConfirm(){
+            if(this.tableData.filter((item)=>{
+              return item.title ===  this.form.title || item.link=== this.form.link
+            }).length>0){
+                this.$message({
+                    message:"invalid input",
+                    type:"error",
+                    showClose:true,
+                    duration: 1500,
+                })
+            }
+            else{
+                this.tableData.push({title:this.form.title,link:this.form.link})
+                this.isShow = false
+            }
+            this.form = { title:"",link:""}
         }
         
     },
@@ -112,23 +153,8 @@
   }
   </script>
   
-  <style scoped>
-  .mask{
-    background-color: rgba(0,0,0,0.5);
-    position: absolute;
-    top:0;left:0;right:0;bottom:0;
-  }
-  
-  .diag{
-    width:400px;
-    height:400px;
-    background-color: orange;
-    position:absolute;
-    top:50%;
-    left:50%;
-    transform:translate(-50%,-50%)
-  }
-  
-  
-  
-  </style>
+ 
+
+
+
+
